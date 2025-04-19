e0e3081f21d8eaedd68f3059d96626861c99398b
Foysal Ahmmed Limon <afaysal410@gmail.com>
2025-04-19 21:13:42 +0600

    refactor DriverSetup

--- src/test/java/utilities/DriverSetup.java
+++ src/test/java/utilities/DriverSetup.java
@@ -1,53 +1,34 @@
 package utilities;
 
 import com.microsoft.playwright.*;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class DriverSetup {
 
-    public static final String BROWSER_NAME = System.getProperty ("browser", "chromium");
-
-    private static final ThreadLocal<Playwright> PLAYWRIGHT_THREAD_LOCAL = new ThreadLocal<> ();
-    private static final ThreadLocal<Browser> BROWSER_THREAD_LOCAL = new ThreadLocal<> ();
-    private static final ThreadLocal<Page> PAGE_THREAD_LOCAL = new ThreadLocal<> ();
-
-    public static Playwright getPlaywrightThreadLocal () {
-        return PLAYWRIGHT_THREAD_LOCAL.get ();
-    }
-
-    public static Browser getBrowserThreadLocal () {
-        return BROWSER_THREAD_LOCAL.get ();
-    }
-
-    public static Page setPageThreadLocal () {
-        return PAGE_THREAD_LOCAL.get ();
-    }
+    protected Browser browser;
+    protected BrowserContext browserContext;
+    protected Page page;
+    Playwright playwright;
+    BrowserType browserType;
 
     @BeforeMethod
-    public void setupBrowserName () {
-        Playwright playwright = Playwright.create ();
-        PLAYWRIGHT_THREAD_LOCAL.set (playwright);
-
-        Browser browser;
-
-        switch (BROWSER_NAME.toLowerCase ()) {
-            case "chromium":
-                browser = playwright.chromium ().launch (new BrowserType.LaunchOptions ().setHeadless (false));
-                break;
-            case "firefox":
-                browser = playwright.firefox ().launch (new BrowserType.LaunchOptions ().setHeadless (false));
-                break;
-            case "webkit":
-                browser = playwright.webkit ().launch (new BrowserType.LaunchOptions ().setHeadless (false));
-                break;
-            default:
-                throw new IllegalArgumentException ("Unsupported browser: " + BROWSER_NAME);
-        }
-
-        BROWSER_THREAD_LOCAL.set (browser);
-
-        Page page = browser.newPage ();
-        PAGE_THREAD_LOCAL.set (page);
+    public void startBrowser () {
+        playwright = Playwright.create ();
+        browserType = playwright.chromium ();
+        browser = playwright.chromium ().launch (new BrowserType.LaunchOptions ().setHeadless (false));
+        browserContext = browser.newContext (new Browser.NewContextOptions ());
+        page = browser.newPage ();
     }
 
+    public void openPage (String baseURL) {
+        page.navigate (baseURL);
+    }
+
+    @AfterMethod
+    public void closeChromeBrowser () {
+        page.close ();
+        browser.close ();
+        playwright.close ();
+    }
 }
